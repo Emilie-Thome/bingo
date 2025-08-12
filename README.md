d96530b18b3f26987b8755a1cb371db758d4a00e
QuCM <57966752+QuCM@users.noreply.github.com>
2025-08-12 10:19:57 +0200

    feat: bingo images generation
    * feat: add image generation
    
    * feat: take a list of names as an input for card names

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ This is the finest Bingo generator.
 ## Usage
 
 ```bash
-python3 bingo.py dummy.txt 10 out
+python3 bingo.py dummy.txt names.txt out
 ```
 
 ## Credit
 
 Sangliana Byte, the best drag queen in algo-rhythm.
+Paula Request, the Flash McQueen of the shady review.
--- bingo.py
+++ bingo.py
@@ -1,16 +1,19 @@
-import sys
 import os
-import numpy as np
 import random as rand
+import sys
+
+import numpy as np
+
+from render import draw_table
 
 
 # Print help for command line usage
 def print_help():
     print(
         "usage: \n\
-  >> python3 bingo.py INPUT_PATH CARDS_NB OUTPUT_DIR\n\
+  >> python3 bingo.py INPUT_PATH NAMES_PATH OUTPUT_DIR\n\
   with - INPUT_PATH: path to the input file containing the text of bingo cells, ONE BY LINE\n\
-       - CARDS_NB:   number of random cards to produce\n\
+       - NAMES_PATH: file with the names of the people participating in the bingo\n\
        - OUTPUT_DIR: path to the output directory where cards will be generated as csv files\n"
     )
 
@@ -34,9 +37,9 @@ def handle_args(args: list[str]) -> tuple[str, int, str]:
     # correct number of arguments?
     if len(args) != 3:
         sys.exit(
-            "input, number of cards, and output paths must be provided, use '-h' for help"
+            "input, file with names, and output paths must be provided, use '-h' for help"
         )
-    return (args[0], int(args[1]), args[2])
+    return (args[0], args[1], args[2])
 
 
 # Get bingo cells content from filepath.
@@ -63,7 +66,7 @@ def generate_cards(cells_nb: int, cards_nb: int) -> list[list[int]]:
 
 
 # Generate files with bingo cards.
-def print_cards(outdir: str, cards: list[list[int]], cells: list[str]):
+def print_cards(outdir: str, cards: list[list[int]], cells: list[str], names: list[str]):
     # create folder if not exist
     if not os.path.exists(outdir):
         os.makedirs(outdir)
@@ -71,20 +74,32 @@ def print_cards(outdir: str, cards: list[list[int]], cells: list[str]):
     cells_nb = len(cells)
     N = int(np.sqrt(cells_nb))
     # function to get the actual cells
-    into_cell = lambda indices: map(lambda index: cells[index], indices)
+    into_cell = lambda indices: list(map(lambda index: cells[index], indices))
     # create `.csv` file for each card
     for n, card in enumerate(cards):
-        with open(outdir + "/card_" + str(n) + ".csv", "w") as file:
-            for i in range(0, cells_nb, N):
-                file.write(",".join(into_cell(card[i : i + N])) + "\n")
+        print(card)
+        card_values = into_cell(card)
+        rows: list[list[str]] = []
+        i = 0
+        for i in range(N):
+            rows.append(card_values[i*N:(i+1)*N])
+        image = draw_table(rows)
+        image.save(os.path.join(outdir, f"{names[n]}.png"), "PNG")
+
+
+def read_names(namepath: str) -> list[str]:
+    with open(namepath, "r") as f:
+        return [n.rstrip().replace(' ', '_') for n in f]
 
 
 def main():
     args = sys.argv[1:]
-    (filepath, cards_nb, outdir) = handle_args(args)
+    (filepath, namepath, outdir) = handle_args(args)
+    names = read_names(namepath)
+    print(names)
     cells = get_cells(filepath)
-    cards = generate_cards(len(cells), cards_nb)
-    print_cards(outdir, cards, cells)
+    cards = generate_cards(len(cells), len(names))
+    print_cards(outdir, cards, cells, names)
 
 
 if __name__ == "__main__":
